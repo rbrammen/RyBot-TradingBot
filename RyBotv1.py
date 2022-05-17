a222b8b5909fc30a20ea9d01bb531bbc1084cafd
Seth Dandridge <sethdan@gmail.com>
2022-05-17 11:30:29 -0400

    Update RyBotv1.py
    1. Group imports by standard library imports, then dependency imports, and then sort the import within those groups alphabetically
    2. Remove the pandas import, you're not using it. The alpha_vantage library looks like it returns a pandas dataframe, you don't need to import pandas to make that happen
    3. Remove hard-coded API key and create exception handling if the environment variable isn't set. Capitalize constants.
    4. Use a context manager (with keyword) for input/output operations like the database so you don't have to explicitly clean up (removes need for call to d.close())
    5. Refactor retrieving and saving to database into functions
    6. Refactor getting the voo prices into a function
    7. Change some variable names to make things clearer
    8. There's almost never a need to declare things as global, just have the function return the values you care about to the calling scope
    9. Moved core routine to if __name__ == __main__: conditional so that if you ever wanted to import the functions into other modules, (like analyzing your database), the purchasing logic wouldn't run.

--- RyBotv1.py
+++ RyBotv1.py
@@ -1,53 +1,59 @@
 import json
+import os
 import shelve
-import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
 import time
 
-d = shelve.open('myfile.db')
-running_cash_balance = d['data'] #change to 10000 to 'reset' the running cash balance / change to d['data'] to keep running total
-#print(running_cash_balance)
-shares_of_voo = d['voo_shares'] #change to 0 to 'reset' the running cash balance / change to d['voo_shares'] to keep running total
-#print(running_cash_balance)
-d.close()
+from alpha_vantage.timeseries import TimeSeries
 
-api_key = '29K8EXDWX6I2DXRM'
+try:
+    ALPHA_VANTAGE_API_KEY = os.environ['ALPHA_VANTAGE_API_KEY']
+except KeyError:
+    raise RuntimeError('ALPHA_VANTAGE_API environmental variable not set, aborting')
 
-ts = TimeSeries(key=api_key, output_format='pandas')
-data, meta_data = ts.get_daily(symbol='VOO', outputsize='compact')
+def get_cash_and_shares():
+    with shelve.open('myfile.db') as database:
+        running_cash_balance = database['data'] #change to 10000 to 'reset' the running cash balance / change to d['data'] to keep running total
+        shares_of_voo = database'voo_shares'] #change to 0 to 'reset' the running cash balance / change to d['voo_shares'] to keep running total
+        return running_cash_balance, shares
 
-closing_price_voo = data['4. close']
-close_price_voo_eod = closing_price_voo[0]
-close_price_voo_eod_prev = closing_price_voo[1]
-#print(closing_price_voo)
-print('Your cash balace is currently:', running_cash_balance)
-print('You currently own', shares_of_voo, 'shares of VOO')
-print('Latest EOD close price of VOO:', close_price_voo_eod)
-print('Previous EOD close price of VOO:', close_price_voo_eod_prev)
+def save_cash_and_shares(running_cash_balance, shares):
+    with shelve.open('myfile.db') as database:
+        database['data'] = running_cash_balance
+        database['voo_shares'] = shares_of_voo
+    
+def get_voo_prices():
+    ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='pandas')
+    data, meta_data = ts.get_daily(symbol='VOO', outputsize='compact')
+    closing_price_voo = data['4. close']
+    close_price_voo_eod = closing_price_voo[0]
+    close_price_voo_eod_prev = closing_price_voo[1]
+    return close_price_voo_eod, close_price_voo_eod_prev
 
-def buy_voo_func():
-    global sod_cash_balance
-    global shares_of_voo 
-    sod_cash_balance = running_cash_balance
-    if shares_of_voo >= 0 and running_cash_balance > closing_price_voo[0] and closing_price_voo[0] < closing_price_voo[1]:
-        sod_cash_balance = sod_cash_balance - closing_price_voo[0]
-        shares_of_voo = shares_of_voo + 1
-        print('You have bought 1 share of VOO today! Your cash balance is now', round(sod_cash_balance))
+def buy_or_sell_voo(cash_balance, shares_of_voo, close_price_voo_eod, close_price_voo_eod_prev):
+    if shares_of_voo >= 0 and cash_balance > close_price_voo_eod and close_price_voo_eod < close_price_voo_eod_prev:
+        cash_balance -= close_price_voo_eod
+        shares_of_voo += 1
+        print('You have bought 1 share of VOO today! Your cash balance is now', round(cash_balance))
         print('You now have', shares_of_voo, 'shares of VOO')
-    elif shares_of_voo > 0 and closing_price_voo[0] > closing_price_voo[1]:
-        sod_cash_balance = sod_cash_balance + closing_price_voo[0]
-        shares_of_voo = shares_of_voo - 1
-        print('You have sold 1 share of VOO today! Your cash balance is now', round(sod_cash_balance))
+    elif shares_of_voo > 0 and close_price_voo_eod > close_price_voo_eod_prev:
+        cash_balance += close_price_voo_eod
+        shares_of_voo -= 1
+        print('You have sold 1 share of VOO today! Your cash balance is now', round(cash_balance))
         print('You now have', shares_of_voo, 'shares of VOO')
-    elif running_cash_balance < closing_price_voo[0]:
+    elif cash_balance < close_price_voo_eod:
         print('You do not have enough money to buy more VOO')
     else:
-         print('You do not have any shares of VOO to sell')
+        print('You do not have any shares of VOO to sell')
+    return cash_balance, shares_of_voo
 
-buy_voo_func()
+if __name__ == "__main__":
+    cash, shares = get_cash_and_shares()
+    close_price_voo_eod, close_price_voo_eod_prev = get_voo_prices()
+    cash, shares = buy_or_sell_voo(cash, shares, close_price_voo_eod, close_price_voo_eod_prev )
 
-running_cash_balance = sod_cash_balance
-d = shelve.open('myfile.db')
-d['data'] = running_cash_balance
-d['voo_shares'] = shares_of_voo
-d.close() 
+    print('Your cash balace is currently:', cash)
+    print('You currently own', shares, 'shares of VOO')
+    print('Latest EOD close price of VOO:', close_price_voo_eod)
+    print('Previous EOD close price of VOO:', close_price_voo_eod_prev)
+
+    save_cash_and_shares(cash, shares)
